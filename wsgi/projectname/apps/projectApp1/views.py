92d6bd9e3747020c17d4e1c66c22143157e91625
murali <murali@localhost.localdomain>
2012-11-15 16:31:05 +0530

    user management

--- wsgi/projectname/apps/projectApp1/views.py
+++ wsgi/projectname/apps/projectApp1/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 from projectApp1.forms import LoginCreateForm
 
 
@@ -16,11 +18,41 @@ def createUser(request):
             user.save()
             newUserCreated = True
         else:
-            formErrors = True
+            pass
+            # form is invalid erros auto set
     else:
         form = LoginCreateForm()
     return render_to_response('loginCreate.html', locals(), context_instance=RequestContext(request))
 
 
-#def login(request):
-#    return render_to_response('login.html', locals(), context_instance=RequestContext(request))
+def siteLogin(request):
+    if request.user.is_authenticated():
+        return redirect('/home/')
+    if request.method == 'POST':
+        form = LoginCreateForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(username=email, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/home/')
+                    #return redirect('http://google.com')
+                    # redirect to success page
+                else:
+                    pass
+                    # redirect ot 'dissabled account'
+            else:
+                wrongUsernameOrPassword = True
+        else:
+            pass
+            # form is invalid erros auto set
+    else:
+        form = LoginCreateForm()
+    return render_to_response('loginCreate.html', locals(), context_instance=RequestContext(request))
+
+
+@login_required(login_url='/login/')
+def home(request):
+    return render_to_response('home.html', locals(), context_instance=RequestContext(request))
