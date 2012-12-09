2ac67b9eb808009748e3aa38872c187b49cfe13d
murali <murali@localhost.localdomain>
2012-12-09 12:37:50 +0530

    corrections

--- wsgi/projectname/apps/TransactionApp/static/timeFilterController.js
+++ wsgi/projectname/apps/TransactionApp/static/timeFilterController.js
@@ -14,29 +14,29 @@
 		};
 		//append query string 
 		$scope.sentTimeRange = function() {
-			$scope.queryString = "?"
-			$scope.tsExist = ((new Date($scope.t_s)).toString() != "Invalid Date")
-			$scope.teExist = ((new Date($scope.t_e)).toString() != "Invalid Date")
+			$scope.queryString = "?";
+			$scope.tsExist = ((new Date($scope.t_s)).toString() != "Invalid Date");
+			$scope.teExist = ((new Date($scope.t_e)).toString() != "Invalid Date");
 			if(!$scope.tsExist && !$scope.teExist){
-				alert("enter valid date")
+				alert("enter valid date");
 				return;
 			}
 			if($scope.tsExist && $scope.teExist){
 				if((new Date($scope.t_s)) > (new Date($scope.t_e))){
-					alert("start date is after the end date duffer")
+					alert("start date is after the end date duffer");
 					return;
 				}
 			}
 			if($scope.tsExist){  
-				$scope.queryString = $scope.queryString.concat("ts=")
-				$scope.queryString = $scope.queryString.concat($scope.t_s) 
-				$scope.queryString = $scope.queryString.concat("&")
+				$scope.queryString = $scope.queryString.concat("ts=");
+				$scope.queryString = $scope.queryString.concat($scope.t_s);
+				$scope.queryString = $scope.queryString.concat("&");
 			}
 			if($scope.teExist){  
-				$scope.queryString = $scope.queryString.concat("te=")
-				$scope.queryString = $scope.queryString.concat($scope.t_e) 
-				$scope.queryString = $scope.queryString.concat("&")
+				$scope.queryString = $scope.queryString.concat("te=");
+				$scope.queryString = $scope.queryString.concat($scope.t_e);
+				$scope.queryString = $scope.queryString.concat("&");
 			}
-			window.location.href = $scope.queryString
+			window.location.href = $scope.queryString;
 		};
 	};
