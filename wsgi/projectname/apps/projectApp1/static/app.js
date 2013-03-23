e5f66795ce5498db3d0de48478320167c9b5472b
murali <murali@localhost.localdomain>
2013-03-24 00:36:59 +0530

    datepicker for angularjs

--- wsgi/projectname/apps/projectApp1/static/app.js
+++ wsgi/projectname/apps/projectApp1/static/app.js
@@ -1,6 +1,6 @@
 /* App Module */
 //replace the angular js markers as they conflict with Django template markers
-angular.module('YourAppName', [], function ($interpolateProvider) {
+angular.module('YourAppName', ['$strap.directives'], function ($interpolateProvider) {
     $interpolateProvider.startSymbol('{[{');
-	$interpolateProvider.endSymbol('}]}');
+    $interpolateProvider.endSymbol('}]}');
 });
